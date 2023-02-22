d9355cac7025b27cbae1499a681086328cbf4109
davidkra230 <davidkra230@gmail.com>
2023-02-22 09:36:55 -0500

    less bugs, harder to read, more deps. but now has user input

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const dns = new dns2();
 const { Packet } = dns2;
 const dnsServer = dns2.createServer({ udp: true });
 const localIp = require("local-ipv4-address");
+const prompt = require("prompt-sync")();
 const app = express();
-
 app.use(express.text());
 
 var ip = null;
 var halt = false;
 var doneSetup = { dns: false, web: false };
+
 localIp().then((yessir) => {
-    ip = yessir;
+    console.log(
+        "(If you have a spare router, you can use it for this process. turn it on, connect, and enter your private ip)"
+    );
+    let input = prompt(
+        `Does "${yessir}" look like the correct ip? (enter for yes, or type in the correct ip): `
+    );
+    if (input == "\n" || input == "" || input == "\r\n" || input == "\n\r") {
+        ip = yessir;
+        return;
+    }
+    ip = input
 });
 
 shenaniganHandler = (req, res) => {
@@ -42,6 +53,7 @@ app.get("*", (_, res) => {
         case "localhost":
         case "[::1]":
         case "junefabrics.com":
+        case "connectivitycheck.gstatic.com":
         /*
             Turi ip ip ip
             Ip ip ip ip tsha ik
@@ -59,60 +71,65 @@ app.get("*", (_, res) => {
 });
 
 // iis had me tripped up, nice.
-app.listen(80, () => {
-    if (halt != true) {
-        console.log("Web Server Listening.");
-    }
-    doneSetup.web = true;
-}).on("error", () => {
-    if (halt != true) {
-        console.log(
-            "The web server failed to start, check if port 80 (http) is in use, if it isn't you can retry with admin privileges."
-        );
-    }
-    halt = true;
-    new Promise(() => {}).then();
-});
+startListening = () => {
+    app.listen(80, () => {
+        if (halt != true) {
+            console.log("Web Server Listening.");
+        }
+        doneSetup.web = true;
+    }).on("error", () => {
+        if (halt != true) {
+            console.log(
+                "The web server failed to start, check if port 80 (http) is in use, if it isn't you can retry with admin privileges."
+            );
+        }
+        halt = true;
+        new Promise(() => {}).then();
+    });
 
-dnsServer.listen({ udp: 53 }).then(() => {
-    if (halt != true) {
-        console.log("DNS Server Listening.");
-    }
-    doneSetup.dns = true;
-});
-
-dnsServer.on("error", () => {
-    if (halt != true) {
-        console.log(
-            "Failed to start the DNS server, you could use SysInternals' TcpView to see what's holding you up, restarting with admin privileges may help."
-        );
-    }
-    halt = true;
-    new Promise(() => {}).then();
-});
-
+    dnsServer.listen({ udp: 53 }).then(() => {
+        if (halt != true) {
+            console.log("DNS Server Listening.");
+        }
+        doneSetup.dns = true;
+    });
+    dnsServer.on("error", () => {
+        if (halt != true) {
+            console.log(
+                "Failed to start the DNS server, you could use SysInternals' TcpView to see what's holding you up, restarting with admin privileges may help."
+            );
+        }
+        halt = true;
+        new Promise(() => {}).then();
+    });
+};
+var startedTMP = false;
 var tempInterval = setInterval(() => {
-    if (ip != null && doneSetup.dns === true && doneSetup.web === true) {
-        clearInterval(tempInterval);
-        tempInterval = undefined;
-        dnsServer.on("request", (request, send, client) => {
-            const response = Packet.createResponseFromRequest(request);
-            const [question] = request.questions;
-            const { name } = question;
-            response.answers = [];
-            response.answers.push({
-                name,
-                type: Packet.TYPE.A,
-                class: Packet.CLASS.IN,
-                ttl: 1,
-                address: ip,
+    if (ip != null) {
+        if (startedTMP == false) startListening();
+        startedTMP = true;
+        if (doneSetup.dns === true && doneSetup.web === true) {
+            clearInterval(tempInterval);
+            tempInterval = undefined;
+            dnsServer.on("request", (request, send, client) => {
+                const response = Packet.createResponseFromRequest(request);
+                const [question] = request.questions;
+                const { name } = question;
+                response.answers = [];
+                response.answers.push({
+                    name,
+                    type: Packet.TYPE.A,
+                    class: Packet.CLASS.IN,
+                    ttl: 1,
+                    address: ip,
+                });
+                send(response);
             });
-            send(response);
-        });
-        console.log(
-            'If everything has loaded any you have accepted the windows network prompt (if any), use the ip: "' +
-                ip +
-                '" as the dns server to start tinkering. (high chance of breaking with multiple connections.)'
-        );
+            console.log(
+                '\n\nIf everything has loaded any you have accepted the windows network prompt (if any), use the ip: "' +
+                    ip +
+                    '" as the dns server to start tinkering.'
+            );
+        }
     }
 }, 25);
